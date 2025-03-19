f43a5c1ddb5653df325314acb190ede76a0a9952
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-19 11:48:59 -0300

    estilizando Item de Navegação

--- src/components/BarraLateral/ItemNavegacao/index.jsx
+++ src/components/BarraLateral/ItemNavegacao/index.jsx
@@ -1,10 +1,19 @@
 import { styled } from 'styled-components'
 
-const ItemEstilizado = styled.li``
+const ItemEstilizado = styled.li`
+  font-size: 24px;
+  line-height: 29px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  color: ${ (props) => props.$ativo ? '#7B78E5' : '#D9D9D9' };
+  display: flex;
+  align-items: center;
+  gap: 22px;
+`
 
 const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
   return (
-    <ItemEstilizado>
+    <ItemEstilizado $ativo={ativo}>
       <img 
         src={ativo ? iconeAtivo : iconeInativo} 
         alt={`Ícone da página ${children}`} 
