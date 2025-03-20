f2024b6d1e501caa136ced7c8878a9fb3a75c90d
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-20 08:10:52 -0300

    adicionando e implementando Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import EstilosGlobais from './components/EstilosGlobais'
 import Cabecalho from './components/Cabecalho'
 import BarraLateral from './components/BarraLateral'
+import Banner from './components/Banner'
+import imagemBanner from './assets/banner.png'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -15,6 +17,10 @@ function App() {
       <EstilosGlobais />
       <Cabecalho />
       <BarraLateral />
+      <Banner
+        backgroundImage={imagemBanner}
+        texto='A galeria mais completa de fotos do espaço!'
+      />
     </FundoGradiente>
   )
 }
