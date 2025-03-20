4e8c83df00bdaf3fa0d7b6e76c69b2adaa066b6a
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-20 08:35:08 -0300

    adicionando componente da Galeria

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cabecalho from './components/Cabecalho'
 import BarraLateral from './components/BarraLateral'
 import Banner from './components/Banner'
 import imagemBanner from './assets/banner.png'
+import Galeria from './components/Galeria'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -19,6 +20,11 @@ const MainContainer = styled.main`
   display: flex;
   gap: 24px;
 `
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 
 function App() {
   return (
@@ -28,10 +34,13 @@ function App() {
         <Cabecalho />
         <MainContainer>
           <BarraLateral />
-          <Banner
-            backgroundImage={imagemBanner}
-            texto='A galeria mais completa de fotos do espaço!'
-          />
+          <ConteudoGaleria>
+            <Banner
+              backgroundImage={imagemBanner}
+              texto='A galeria mais completa de fotos do espaço!'
+            />
+            <Galeria />
+          </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
     </FundoGradiente>
