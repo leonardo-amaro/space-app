b6e6ee1043cf3f7da6218a4dc2ed8e9eb882a028
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-22 18:41:54 -0300

    estilizando modal

--- src/components/Galeria/Imagens/index.jsx
+++ src/components/Galeria/Imagens/index.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import BotaoIcone from '../../BotaoIcone'
 
 const Figure = styled.figure`
-  width: ${(props) => props.$expandida ? '90%' : '400px'};
+  width: ${(props) => props.$expandida ? '100%' : '400px'};
   max-width: 100%;
   margin: 0;
   display: flex;
--- src/components/ModalZoom/index.jsx
+++ src/components/ModalZoom/index.jsx
@@ -1,11 +1,47 @@
+import styled from "styled-components"
+import Imagens from "../Galeria/Imagens"
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.6);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`
+const DialogEstilizado = styled.dialog`
+  position: absolute;
+  top: 300px;
+  width: 80%;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  & form {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    & button {
+      cursor: pointer;
+      border: none;
+      background-color: transparent;
+    }
+  }
+`
+
 const ModalZoom = ({ foto }) => {
   return (
-    <dialog open={!!foto}>
-      <p>Exemplo do Mozilla</p>
-      <form method="dialog">
-        <button type="submit">ok</button>
-        </form>
-    </dialog>
+    foto && 
+    <>
+      <Overlay />
+      <DialogEstilizado open={!!foto}>
+        <Imagens foto={foto} expandida={true} />
+        <form method="dialog">
+          <button type="submit">
+            <img src="/icones/fechar.png" alt="Icone para fechar modal" />
+          </button>
+          </form>
+      </DialogEstilizado>
+    </>
   )
 }
 
